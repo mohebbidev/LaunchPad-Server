3d281fe6a09fb68c4b251e3c1e7b532d8bbc3922
NooberThanYall <loudervibes@gmail.com>
2026-06-18 21:11:56 +0330

    added log registry

--- internal/application/log_registry.go
+++ internal/application/log_registry.go
@@ -6,3 +6,24 @@ type LogRegistry struct {
 	m sync.Map
 }
 
+
+func NewLogRegistry() *LogRegistry {
+	return &LogRegistry{}
+}
+
+func (r *LogRegistry) Register(projectID string, ch chan LogLine) {
+	r.m.Store(projectID, ch)
+}
+ 
+func (r *LogRegistry) Get(projectID string) (chan LogLine, bool) {
+	v, ok := r.m.Load(projectID)
+	if !ok {
+		return nil, false
+	}
+	return v.(chan LogLine), true
+}
+ 
+func (r *LogRegistry) Delete(projectID string) {
+	r.m.Delete(projectID)
+}
+ 
